57e29e5842a70431207abe71135b4f1f31a34376
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-06-01 17:13:06 -0700

    initial commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,37 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    buzzwordCounter: 0,
+    solidNav: false,
+    buzzwords: [
+      "machine learning",
+      "data visualization",
+      "scalable computing",
+      "big data"
+    ],
+    show: {
+      mobilenav: false
+    }
+  },
+  methods: {
+    trackPos: function() {
+      var el = document.querySelector("#mainexpl");
+      var pos = getPosition(el);
+      if (pos.y <= 0) { this.solidNav = true; }
+      else if (this.solidNav === true) { this.solidNav = false; }
+    }
+  },
+  computed: {
+    currentBuzzword: function () {
+      if (this.buzzwordCounter >= this.buzzwords.length) {
+        this.buzzwordCounter = 0;
+      }
+      return this.buzzwords[this.buzzwordCounter]
+    }
+  },
+  mounted: function () {
+    setInterval(function () {
+      this.buzzwordCounter += 1
+    }.bind(this), 3000)
+  }
+})
