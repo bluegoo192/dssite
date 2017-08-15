a546eb3b5c66ec90e6bf978ce0f3d3abac98b9f4
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-08-14 22:01:58 -0700

    stub routes, people page in progress

---
+++ public/javascripts/people.js
@@ -0,0 +1,58 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    solidNav: true,
+    show: {
+      mobilenav: false
+    },
+    officers: [
+      {
+        name: "Jason Freeberg",
+        file: "jason_freeberg.jpg",
+        position: "President",
+        description: "JSON"
+      },
+      {
+        name: "Arthur Silverstein",
+        file: "arthur_silverstein.jpg",
+        position: "Director of IT",
+        description: "oh dude hell yea!"
+      },
+      {
+        name: "Timothy Nguyen",
+        file: "timothy_nguyen.jpg",
+        position: "Bus driver",
+        description: "never wears pants"
+      },
+      {
+        name: "Samantha Lee",
+        file: "samantha_lee.jpg",
+        position: "Asswipe",
+        description: "Recently turned 21"
+      },
+      {
+        name: "Shon SomethingOrOther",
+        file: "shon_lastname.jpg",
+        position: "Arthur's Assistant",
+        description: "Likes coconuts"
+      },
+      {
+        name: "Cindy SomethingOrOther",
+        file: "cindy_lastname.jpg",
+        position: "Out of town",
+        description: "Codes"
+      },
+      {
+        name: "Richa SomethingOrOther",
+        file: "richa_lastname.jpg",
+        position: "I don't know",
+        description: "Works for Invoca!"
+      }
+    ]
+  },
+  methods: {
+    getOfficerPath: function (filename) {
+      return "/images/officers/"+filename
+    }
+  }
+})
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
 router.get('/test', function(req, res, next) {
-  res.render('test', { title: 'Express' });
+  res.render('test', { title: 'Color Test' });
 });
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+router.get('/people', function(req, res, next) {
+  res.render('people', { title: 'People' });
+});
+
+router.get('/events', function(req, res, next) {
+  res.render('events', { title: 'Events' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+router.get('/datatalks', function(req, res, next) {
+  res.render('datatalks', { title: 'Data Talks' });
+});
+
+
+
+
+
 
 module.exports = router;
