cf4cb913dbe112acb6bc146efd16893f062a9562
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-08-20 02:54:35 -0700

    very basic styles

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -4,10 +4,16 @@ var app = new Vue({
     solidNav: true,
     posts: [
       {
-        name: "Test post please ignore",
+        title: "Test post please ignore",
         author: "Rick Sanchez",
         date: "April 20, 1969",
         content: "blah blah blah this is a sample blog post.  I wrote it on the airplane back from Seoul. Wow writing sucks, can't imagine why anyone does it.  Gonna stop now"
+      },
+      {
+        title: "Another sample post",
+        author: "Yancellor Chang",
+        date: "April 20, 1969",
+        content: "Holy hell I can't believe I have to write another one of these stupid sample posts. God fucking dammit lol.  I can't even get some lorem ipsum because this plane has no internet and Pages won't let my copy paste it's lorem ipsum for some reason.  Also I just realized, storing this content as a string is going to be pretty troublesome.  Gonna have to deal with newlines, quotation marks, backslashes..."
       }
     ],
     show: {
