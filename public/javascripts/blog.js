0243c5fac7fa1f89ee648b19217055a251ff1f9c
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-08-22 23:52:26 -0700

    better blog resizing

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -21,6 +21,12 @@ var app = new Vue({
         author: "Beans",
         date: "April 20, 1969",
         content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets"
+      },
+      {
+        title: "Yet another sample",
+        author: "Beans",
+        date: "April 20, 1969",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets"
       }
     ],
     show: {
