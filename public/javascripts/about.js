ac2d5c182045375dce6b5acf65bb0f927597cb1d
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-09-21 14:15:47 -0700

    images on carousel

--- public/javascripts/about.js
+++ public/javascripts/about.js
@@ -12,26 +12,32 @@ var app = new Vue({
     whatwedos: [
       {
         name: "Projects",
+        image: "images/projects.jpg",
         description: "A blurb about our projects"
       },
       {
         name: "Workshops",
+        image: "images/sql_workshop_2.jpg",
         description: "A blurb about our Workshops"
       },
       {
         name: "Data Talks",
+        image: "images/data_talk.jpg",
         description: "A blurb about our Data Talks"
       },
       {
         name: "Networking Events",
+        image: "images/graphiq.jpg",
         description: "A blurb about our Networking Events"
       },
       {
         name: "Hackathons",
+        image: "images/hackathon.jpg",
         description: "A blurb about our Hackathons"
       },
       {
         name: "Consulting",
+        image: "images/consulting.jpg",
         description: "A blurb about our Consulting"
       }
     ]
