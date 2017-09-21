c12e6ecd5d1b413c0b26c0e431e12b636825599a
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-09-21 13:29:24 -0700

    basic carousel

--- public/javascripts/about.js
+++ public/javascripts/about.js
@@ -3,7 +3,40 @@ var app = new Vue({
   data: {
     transparentNav: false,
     show: {
-      mobilenav: false
-    }
+      mobilenav: false,
+      active: {
+        name: "Projects",
+        description: "A blurb about our projects"
+      }
+    },
+    whatwedos: [
+      {
+        name: "Projects",
+        description: "A blurb about our projects"
+      },
+      {
+        name: "Workshops",
+        description: "A blurb about our Workshops"
+      },
+      {
+        name: "Data Talks",
+        description: "A blurb about our Data Talks"
+      },
+      {
+        name: "Networking Events",
+        description: "A blurb about our Networking Events"
+      },
+      {
+        name: "Hackathons",
+        description: "A blurb about our Hackathons"
+      },
+      {
+        name: "Consulting",
+        description: "A blurb about our Consulting"
+      }
+    ]
+  },
+  mounted: function () {
+    this.show.active = this.whatwedos[0];
   }
 })
