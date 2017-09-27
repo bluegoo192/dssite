79881f8087d403d646ee8533b8e5dd6b9a512017
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-09-26 20:08:15 -0700

    added more officers

--- public/javascripts/people.js
+++ public/javascripts/people.js
@@ -16,24 +16,101 @@ var app = new Vue({
         current: false
       },
       {
-        name: "Arthur Silverstein",
-        file: "arthur_silverstein.jpg",
-        position: "Director of IT",
-        description: "oh dude hell yea!",
-        current: false
-      },
-      {
-        name: "Timothy Nguyen",
-        file: "timothy_nguyen.jpg",
-        position: "Bus driver",
-        description: "never wears pants",
+        name: "Raul Eulogio",
+        file: "samantha_lee.jpg",
+        position: "Vice President",
+        description: "JSON",
         current: false
       },
       {
         name: "Samantha Lee",
         file: "samantha_lee.jpg",
-        position: "Asswipe",
+        position: "Vice President",
         description: "Recently turned 21",
+        current: false
+      },
+      {
+        name: "Ryan Poff",
+        file: "arthur_silverstein.jpg",
+        position: "Finance",
+        description: "JSON",
+        current: false
+      },
+      {
+        name: "Moritz Droste",
+        file: "shon_lastname.jpg",
+        position: "Finance",
+        description: "JSON",
+        current: false
+      },
+      {
+        name: "Ananya Havaru",
+        file: "timothy_nguyen.jpg",
+        position: "Finance",
+        description: "JSON",
+        current: false
+      },
+      {
+        name: "Calvin Wang",
+        file: "calvin_wang.jpg",
+        position: "Something about marketing",
+        description: "The Wang",
+        current: false
+      },
+      {
+        name: "Timothy Nguyen",
+        file: "timothy_nguyen.jpg",
+        position: "Marketing",
+        description: "never wears pants",
+        current: false
+      },
+      {
+        name: "Jonathan Hsu",
+        file: "samantha_lee.jpg",
+        position: "Marketing",
+        description: "JSON",
+        current: false
+      },
+      {
+        name: "Richa Wadaskar",
+        file: "richa_lastname.jpg",
+        position: "Marketing",
+        description: "Works for Invoca!",
+        current: false
+      },
+      {
+        name: "Holly Cheek",
+        file: "jason_freeberg.jpg",
+        position: "Operations",
+        description: "JSON",
+        current: false
+      },
+      {
+        name: "Matthew Peterschmidt",
+        file: "samantha_lee.jpg",
+        position: "Operations",
+        description: "JSON",
+        current: false
+      },
+      {
+        name: "Daniel Fields",
+        file: "timothy_nguyen.jpg",
+        position: "Operations",
+        description: "JSON",
+        current: false
+      },
+      {
+        name: "Eri Kawakami",
+        file: "cindy_lastname.jpg",
+        position: "Operations",
+        description: "JSON",
+        current: false
+      },
+      {
+        name: "Arthur Silverstein",
+        file: "arthur_silverstein.jpg",
+        position: "Director of IT",
+        description: "oh dude hell yea!",
         current: false
       },
       {
@@ -48,20 +125,6 @@ var app = new Vue({
         file: "cindy_lastname.jpg",
         position: "Out of town",
         description: "Codes",
-        current: false
-      },
-      {
-        name: "Richa SomethingOrOther",
-        file: "richa_lastname.jpg",
-        position: "I don't know",
-        description: "Works for Invoca!",
-        current: false
-      },
-      {
-        name: "Calvin Wang",
-        file: "calvin_wang.jpg",
-        position: "Something about marketing",
-        description: "The Wang",
         current: false
       }
     ]
