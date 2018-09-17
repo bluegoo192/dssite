84c451747052ae8114ef4610a4d67fd52334d6b7
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-09-17 12:20:32 -0700

    postgresclient

---
+++ database/postgresclient.js
@@ -0,0 +1,5 @@
+/* This module encapsulates connection to the Postgres database.
+  Right now, we use this database for managing users.
+  PSQL connection command, for manual editing:
+  psql --port=5432 --host=ucsb-data-science.postgres.database.azure.com --username=dsadmin@ucsb-data-science -W
+*/
