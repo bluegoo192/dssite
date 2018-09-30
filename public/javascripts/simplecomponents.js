8925ff3b92fb75cfc8bec220322c6552611c8e4a
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-09-30 13:00:39 -0700

    login

--- public/javascripts/simplecomponents.js
+++ public/javascripts/simplecomponents.js
@@ -20,7 +20,7 @@ Vue.component('login', {
   `<div>
       <a @click="showModal = true">Log In</a>
       <modal title="Log In" :show="showModal" @closeModalEvent="closeModal">
-        <form>
+        <form action="/login" method="post">
           <input :class="{invalidInput: !emailIsValid}" type="email" name="email" v-model="email">
           <input type="password" name="password" v-model="password">
           <input type="submit" value="Submit">
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,49 @@
 var express = require('express');
 var router = express.Router();
-var database = require('../database/mongooseclient.js');
+var mongo = require('../database/mongooseclient.js');
+var db = require('../database/sqlclient.js')
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+  {usernameField: 'email', passwordField: 'password'},
+  function(username, password, done) {
+    console.log('username', username);
+    console.log('password', password);
+    db.checkLogin({email: username, password}).then(result => {
+      return done(null, result);
+    }).catch(err => {
+      return done(err);
+    });
+  }
+));
+
+const isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+  res.redirect('/');
+};
+
+passport.serializeUser(function(user, done) {
+  // please read the Passport documentation on how to implement this. We're now
+  // just serializing the entire 'user' object. It would be more sane to serialize
+  // just the unique user-id, so you can retrieve the user object from the database
+  // in .deserializeUser().
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  // Again, read the documentation.
+  done(null, user);
+});
+
+const render = function (page) {
+  return function (req, res, next) {
+    res.render(page, { loggedIn: req.user != null, user: req.user });
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
-});
+router.get('/', render('index'));
 
 router.get('/test', function(req, res, next) {
   res.render('test', { title: 'Color Test' });
@@ -36,8 +74,30 @@ router.get('/datatalks', function(req, res, next) {
 });
 
 router.post('/api/v1/createBlogPost', async function(req, res, next) {
-  var status = await database.createBlogPost(req.body);
+  var status = await mongo.createBlogPost(req.body);
   res.send(status);
+});
+
+router.get('/resources', isAuthenticated, async function(req, res, next) {
+  res.render('about');
+})
+
+// router.post('/login', function(req, res, next) {
+//   passport.authenticate('local', function(err, user, info) {
+//     console.log(req.body);
+//     if (err) { return next(err); }
+//     if (!user) { return res.redirect('/login'); }
+//     req.logIn(user, function(err) {
+//       if (err) { return next(err); }
+//       return res.redirect('/');
+//     });
+//   })(req, res, next);
+// });
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/about',
+}), function(req, res, next) {
+  res.render('index');
 });
 
 
