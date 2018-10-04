7c35b0b27be0f6e8e04aed457e780f80bf8d722f
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-10-03 18:07:42 -0700

    stub getRole function

---
+++ utils/checkRole.js
@@ -0,0 +1,8 @@
+// given a member ID, retrieve a string representing their role/permissions
+// returns null for generic members (no special role)
+// possible roles can be found in permissions.role in the sql db
+const getRole = (memberId) => {
+  return null;
+}
+
+module.exports = getRole;
