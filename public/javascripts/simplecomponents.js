cba9bd35095721daca333d3a09e703c8586328ed
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-10-15 14:10:08 -0700

    progress

--- public/javascripts/simplecomponents.js
+++ public/javascripts/simplecomponents.js
@@ -1,5 +1,6 @@
 Vue.component('nav-hamburger', {
-  template: '<div id="hamburger" class="vcentercontainer" @click="togglenav">\
+  props: ['onClick'],
+  template: '<div id="hamburger" class="vcentercontainer" @click="onClick">\
               <div class="flex">\
                 <a class="vcentercontainer" href="#">\
                   <svg height="32px" id="Layer_1" style="enable-background:new 0 0 32 32;" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z"/></svg>\
@@ -10,6 +11,16 @@ Vue.component('nav-hamburger', {
   methods: {
     togglenav: function () {
       this.$emit('togglenav');
+    }
+  }
+});
+
+Vue.component('navbar', {
+  data: function () {
+    return {
+      showMobileNav: false,
+      transparentBackground: false,
+      testmsg: 'hi',
     }
   }
 });
