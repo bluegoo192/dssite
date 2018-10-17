45a7c512a12a940c91117ddc2f05b78f16a70ce4
bluegoo192 <silversteinarthur+git@gmail.com>
2018-10-16 18:11:20 -0700

    switch cameras

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -10,6 +10,8 @@ var app = new Vue({
     loading: false,
     scanError: null,
     scanContent: null,
+    currentCamera: 0,
+    scanner: null,
   },
   methods: {
     downloadMembers: function () {
@@ -22,6 +24,7 @@ var app = new Vue({
     initScanner: function () {
       this.showScanner = true;
       let scanner = new Instascan.Scanner({ video: document.getElementById('scanner') });
+      this.scanner = scanner;
       console.log('created scanner')
       scanner.addListener('scan', function (content) {
         this.scanContent = content;
@@ -29,7 +32,7 @@ var app = new Vue({
       console.log('added listener')
       Instascan.Camera.getCameras().then(cameras => {
         if (cameras.length > 0) {
-          scanner.start(cameras[0]);
+          scanner.start(cameras[this.currentCamera]);
         } else {
           console.log('no cameras')
           this.scanError = "No camera found";
@@ -38,6 +41,21 @@ var app = new Vue({
         console.log('unexpected error')
         this.scanError = e;
       });
-    }
+    },
+    switchCameras: function () {
+      if (this.scanner == null) return;
+      this.currentCamera = this.currentCamera + 1;
+      Instascan.Camera.getCameras().then(cameras => {
+        if (cameras.length > 0) {
+          scanner.start(cameras[this.currentCamera % cameras.length]);
+        } else {
+          console.log('no cameras')
+          this.scanError = "No camera found";
+        }
+      }).catch(e => {
+        console.log('unexpected error')
+        this.scanError = e;
+      });
+    },
   }
 })
